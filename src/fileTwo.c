4ad6a41de05bb571686dbc83bf56399b5fd9f266
Sudhakar Palanivelu <sudhakar.p1@gmail.com>
2022-03-02 19:48:41 +0530

    Adding src folder

---
+++ src/fileTwo.c
@@ -0,0 +1,89 @@
+Good Evening Guys...
+====================
+	I am Sudhakar Palanivelu
+	=========================
+		C, C++, Python , UNIX/Linux...
+	
+========================================================
+	git ---> Tool introduced Linus Torvalds --> 2005
+	-------------------------------------------------
+	
+	Version Control System
+	-----------------------
+		Keeping track of all the changes that were done between 2 version of the same software.
+		
+		1. Centralized Versioning System. 
+					svn --> Sub-Versioning 
+					Team Foundation 
+					
+			A Single centralized version server maintaining the files, data base needed for managing and tracking all the changes done to the software by any number of people in the project.
+			
+			If the centralized server break down, people working on the project will not be able access latest updates.
+			
+					
+		2. Distributed Versioning System
+				git 
+					
+		that you have a local copy of your software, as well as a copy in the server.
+
+==========================================================
+Websites which act as a servers for you git tool/command, where you can keep your files on any of these servers as well.
+
+	GitHub --> Purchased by Microsoft
+	GitLab
+	Bitbucket
+===========================================================
+
+git as a tool can be used as client or server tools which can be used to store and manage your project effectively.
+	goto the following link to download and install git for windows
+	
+		https://git-scm.com/download/win
+
+	1. CLI ---> 
+		gitBash, git for windows
+	2. GUI ---> 
+		gitDesktop, gitUp, gitKraken... etc...
+		
+	
+	folders and files involved in your project ---> managing all the given structure is call git tree.
+	
+	
+Initialize Git
+	git init --> out 
+	git init projectName --> main project folder
+		it creates an environment for managing and tracking our project. 
+		
+		this we call it a initializing a new project folder structure or repository
+	
+	=============================================
+	what ever changes happen inside your working directory/folder (repository)
+		these changes has to tracked or ignored by your git has to be said by the programmer.
+		
+	
+
+
+
+
+	
+	
+	
+	
+
+
+
+
+
+			
+			
+			
+			
+			
+			
+			
+	
+	
+	
+	
+	
+	
+		
